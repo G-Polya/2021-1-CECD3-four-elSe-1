40e94601831a641ff3a35fdb105842b4bfbc092c
George-Polya <wlskaka4@gmail.com>
2021-08-19 05:01:25 -0400

    file upload

--- integrated_main/detectionAPI.py
+++ integrated_main/detectionAPI.py
@@ -8,7 +8,7 @@ from werkzeug.utils import secure_filename
 
 import tensorflow as tf
 
-from EfficientDet import *
+from yolov5 import detect
 import os
 
 
@@ -16,75 +16,65 @@ import os
 app = Flask(__name__)
 api = Api(app)
 
+# 업로드 HTML 렌더링
 @app.route("/upload")
 def render_file():
     return render_template("upload.html")
 
+
+# 파일 업로드 처리
 @app.route("/fileUpload", methods=["GET", "POST"])
 def upload_file():
     if request.method =="POST":
         f = request.files["file"]
-        filename = "./original_test/" + secure_filename(f.filename)
+        filename = "./yolov5/hanssem/images/test/" + secure_filename(f.filename)
         f.save(filename)
-        return "original_test 디렉터리 -> 파일 업로드 성공!"
+        return "hanssem/images/test/ 디렉터리 -> 파일 업로드 성공!"
 
 
 
 
-class Detector(Resource):
-    detector = None
+# class Detector(Resource):
+#     detector = None
     
-    def __new__(cls, *args, **kwargs):
-        if not hasattr(cls, "_instance"):
-            # print("__new__ is called")
-            cls._instance = super(Detector,cls).__new__(cls)
-        return cls._instance
+#     def __new__(cls, *args, **kwargs):
+#         if not hasattr(cls, "_instance"):
+#             # print("__new__ is called")
+#             cls._instance = super(Detector,cls).__new__(cls)
+#         return cls._instance
 
-    def __init__(cls,module_handle="https://tfhub.dev/tensorflow/efficientdet/d0/1"):
-        if cls.detector != None:
-            print("Already exist")
-        else:
-            cls.detector =  hub.load(module_handle)
+#     def __init__(cls,module_handle="https://tfhub.dev/tensorflow/efficientdet/d0/1"):
+#         if cls.detector != None:
+#             print("Already exist")
+#         else:
+#             cls.detector =  hub.load(module_handle)
 
-    def get(self):
-        pass
+#     def get(self):
+#         pass
 
-    def getDetector(cls):
-        return cls.detector
+#     def getDetector(cls):
+#         return cls.detector
 
 
 
-@app.route("/api")
-class EfficientDet(Resource):
+# @app.route("/api")
+# class EfficientDet(Resource):
 
-    def get(self):
-        model =  Detector().getDetector()
+#     def get(self):
+#         model =  Detector().getDetector()
         
-        # dataset_path = "./original_train_person/"
-        dataset_path = "./original_test/"
-        output_path = "./detected_data/detected_from_test/"
-        dataset_list = os.listdir(dataset_path)
-        detected_objectList = object_detection(model, dataset_list,dataset_path, output_path) # detection에 약 8초
+#         # dataset_path = "./original_train_person/"
+#         dataset_path = "./original_test/"
+#         output_path = "./detected_data/detected_from_test/"
+#         dataset_list = os.listdir(dataset_path)
+#         detected_objectList = object_detection(model, dataset_list,dataset_path, output_path) # detection에 약 8초
        
-        return jsonify({"detected_objectList" : detected_objectList, "model":str(model)})
+#         return jsonify({"detected_objectList" : detected_objectList, "model":str(model)})
 
-api.add_resource(EfficientDet, "/api/efficientDet")
-api.add_resource(Detector, "/")
+# api.add_resource(EfficientDet, "/api/efficientDet")
+# api.add_resource(Detector, "/")
 
 
 if __name__ =="__main__":
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    if gpus:
-        # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
-        try:
-            tf.config.experimental.set_virtual_device_configuration(
-                gpus[0],
-                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024 * 10)])
-
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Virtual devices must be set before GPUs have been initialized
-            print(e)
     app.run()
     
--- readme.md
+++ readme.md
@@ -1,6 +1,12 @@
-# 훈련
+# Train
 /integrated_main/yolov5에서 
 - single GPU : python train.py --batch 64 --data hanssem.yaml --weights yolov5s.pt --device 0
 - multi GPU : python -m torch.distributed.launch --nproc_per_node 2 train.py --batch 64 --data hanssem.yaml --weights yolov5s.pt --device 0,1
+
+# 검색대상이 될 retrieval object pool 생성
+- retrieval object pool은 검색을 수행하기 전에 미리 만들어놔야한다
+
+# inference and retrieval
+- inference.ipynb에서 실행
 
 # 
