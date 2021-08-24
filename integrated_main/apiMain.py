54168d8eefa4abc6b52df831e608789be5c85d48
George-Polya <wlskaka4@gmail.com>
2021-08-24 07:19:25 -0400

    delete code added

--- integrated_main/apiMain.py
+++ integrated_main/apiMain.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request,jsonify
 
 
 from werkzeug.utils import secure_filename
-
+import os
 
 
 app = Flask(__name__)
@@ -25,11 +25,29 @@ def upload_file():
         filename = "./yolov5/hanssem/images/query/" + secure_filename(f.filename)
         f.save(filename)
         return "/yolov5/hanssem/images/query/ 디렉터리 -> 파일 업로드 성공!"
+class Delete(Resource):
+    def get(self):
+        os.chdir("./yolov5/hanssem/images/query/")
+        print("current directory : ",os.getcwd())
+        os.system("rm *.jpg")
+
+        os.chdir("../../../runs")
+        print("current directory : ",os.getcwd())
+        os.system("rm -rf detect/")
+
+        os.chdir("../../../")
+        print("current directory : ",os.getcwd())
+        
+        print("delete completed")
+        return "delete completed"
+        
 
 
 api.add_resource(ModelLoader,"/")
 api.add_resource(Detection,"/api/detection")
 api.add_resource(Select, "/api/select")
 api.add_resource(Retrieval, "/api/retrieval")
+api.add_resource(Delete,"/delete")
+
 if __name__ == "__main__":
     app.run(debug=True)
